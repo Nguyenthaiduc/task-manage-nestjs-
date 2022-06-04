c6054081d4154bba8f8c6c6f6af85669097fc4e1
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-06-04 13:59:34 +0700

    Delete task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { timeStamp } from 'console';
 
 @Injectable()
 export class TasksService {
@@ -29,5 +28,9 @@ export class TasksService {
     this.tasks.push(task);
 
     return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
