007916879bb0272f02e68262e4c0e9c63f701840
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-06-04 19:30:50 +0700

    Repository Task

---
+++ src/tasks/tasks.repository.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import { CustomRepository } from "src/database/typeorm-ex.decorator";
+import { EntityRepository, Repository } from "typeorm";
+import { Task } from "./task.entity";
+
+@EntityRepository(Task)
+@CustomRepository(Task)
+export class TasksRepository extends Repository<Task> {
+
+}
