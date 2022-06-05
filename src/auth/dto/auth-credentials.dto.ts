887bf1008bc387a8d1511fba2a24df416e463775
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-06-05 10:24:22 +0700

    Feature_ Signing Up

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,5 @@
+/* eslint-disable */
+export class AuthCredentialsDto {
+    username : string;
+    password : string;
+}
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,8 +1,14 @@
 /* eslint-disable */
-import { EntityRepository, Repository } from "typeorm";
-import { User } from "./user.entity";
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+  async createuser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
     
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
 }
