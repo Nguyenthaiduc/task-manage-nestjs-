c63e7e10275e15dd8522220867a4202853846d78
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-06-05 21:12:18 +0700

    Implementing Logs in our NestJS app

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -5,9 +5,12 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './task-status.enum';
 import { Task } from './task.entity';
+import { InternalServerErrorException, Logger } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
+  private logger = new Logger('TaskRepository', true);
+
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
 
@@ -27,12 +30,21 @@ export class TasksRepository extends Repository<Task> {
 
     //Clean your room -> clean your room
     //CLEAN -> clean
-
-    const tasks = await query.getMany();
-    return tasks;
+    try {
+      const tasks = await query.getMany();
+      return tasks;
+    } catch (error) {
+      this.logger.error(
+        `Failed to get tasks for user "${
+          user.username
+        }". Filter: ${JSON.stringify(filterDto)}`,
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
   }
 
-  async createTask(createTaskDto: CreateTaskDto, user:User): Promise<Task> {
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     const { title, description } = createTaskDto;
 
     const task = this.create({
